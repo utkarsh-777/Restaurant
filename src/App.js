0693cb40308bdfb96649a37242468ea042a49976
Utkarsh Kashyap <kumarutkarsh305@gmail.com>
2022-04-14 01:50:43 +0530

    landing page work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './css/style.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LandingPage from './Pages/LandingPage/LandingPage';
+import AdminLogin from './Pages/AdminLogin/AdminLogin';
 
-function App() {
+const Routing = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<LandingPage />} />
+        <Route exact path='/admin-login' element={<AdminLogin />} /> 
+      </Routes>
+    </Router>
+  )
+}
+
+const App = () => {
+  return (
+    <Routing />
   );
 }
 
---
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import p1 from "../../images/p1.png";
+import p2 from "../../images/p2.png";
+import p3 from "../../images/p3.png";
+import p4 from "../../images/p4.png";
+
+const LandingPage = () => {
+  return (
+    <div className="main">
+      <div className="landingPage">
+        <div className="landingPage__header">Lemon</div>
+        <div className="landingPage__main">
+          <div style={{ display: "flex" }}>
+            <div className="landingPage__main--heading1">
+              It’s not just Food.
+              <br />
+              It’s an
+              <br /> Experience!
+            </div>
+            <div className="landingPage__main--divider">
+              <div className="landingPage__main--divider-1"></div>
+              <div className="landingPage__main--divider-2"></div>
+              <div className="landingPage__main--divider-3"></div>
+            </div>
+            <div className="landingPage__main--heading2">
+              Indulge into our fine dining
+              <br /> with chef special dishes,
+              <br /> drinks and more.....
+              <br />
+              <a
+                href="#any"
+                style={{ fontWeight: "bold" }}
+                className="btn btn--white btn--animated u-margin-top-medium"
+              >
+                explore&nbsp;&nbsp;&nbsp;&rarr;
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="landingPage2">
+        <div className="landingPage2__main">
+          <div className="row">
+            <div className="col-1-of-4">
+              <div className="landingPage2__main--imgdiv">
+                <img className="landingPage2__main--p1" src={p1} alt="p1" />
+              </div>
+            </div>
+            <div className="col-1-of-4">
+              <div className="landingPage2__main--imgdiv">
+                <img className="landingPage2__main--p2" src={p2} alt="p2" />
+              </div>
+            </div>
+            <div className="col-1-of-4">
+              <div className="landingPage2__main--imgdiv">
+                <img className="landingPage2__main--p3" src={p3} alt="p3" />
+              </div>
+            </div>
+            <div className="col-1-of-4">
+              <div className="landingPage2__main--imgdiv">
+                <img className="landingPage2__main--p4" src={p4} alt="p4" />
+              </div>
+            </div>
+          </div>
+          <br/>
+          <div className="row" style={{marginTop:'-10rem'}}>
+            <div className="col-1-of-4">
+              <div className="landingPage2__main--heading1">
+                Best in class <br /> Dining Experience
+              </div>
+            </div>
+            <div className="col-1-of-4">
+              <div className="landingPage2__main--heading2">Take away</div>
+            </div>
+            <div className="col-1-of-4">
+              <div className="landingPage2__main--heading3">
+                Best Food Quality
+              </div>
+            </div>
+            <div className="col-1-of-4">
+              <div className="landingPage2__main--heading4">
+                Best in Food <br/>Safety
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
